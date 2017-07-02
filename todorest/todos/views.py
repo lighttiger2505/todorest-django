b739415accb5cdc54b334020ab221bdfd8d4e1ba
lighttiger <lighttiger2505@gmail.com>
2017-07-03 06:27:25 +0900

    Fix mixin to builtin generic view.

--- todorest/todos/views.py
+++ todorest/todos/views.py
@@ -1,36 +1,15 @@
 # -*- coding: utf-8 -*-
 from rest_framework import generics
-from rest_framework import mixins
 
 from todos.models import Todo
 from todos.serializers import TodoSerializer
 
 
-class TodoList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+class TodoList(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, args, kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, args, kwargs)
-
-
-class TodoDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, args, kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, args, kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, args, kwargs)
